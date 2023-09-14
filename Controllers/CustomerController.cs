aa21a79b14f6311e96c214c9ae2dce587242c413
raisircl <rai.verma@live.com>
2023-09-14 19:06:09 +0530

    Dapper CRUD Done

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -18,5 +18,69 @@ namespace WebAppMVCDapper.Controllers
             //return Json(customers);
             return View(customers);
         }
+        public IActionResult Details(int id)
+        {
+            string constr = @"Data Source=.\sqlexpress;Initial Catalog=sircldb;Persist Security Info=True;User ID=sa;Password=rai11**";
+            Customer? customer = new Customer();  
+            IDbConnection conn = new SqlConnection(constr);
+            customer = conn.Query<Customer>($"select * from customers where customerid={id}").SingleOrDefault();
+            if(customer!=null)
+            {
+                return View(customer);
+            }
+            return View(null);
+            //return Json(customers);
+            
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Customer customer)
+        {
+            string constr = @"Data Source=.\sqlexpress;Initial Catalog=sircldb;Persist Security Info=True;User ID=sa;Password=rai11**";
+            
+            IDbConnection conn = new SqlConnection(constr);
+            string cmd = "insert into customers(firstname,lastname,email) values(@FirstName,@LastName,@Email)";
+            int affectedrows=conn.Execute(cmd, customer);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            string constr = @"Data Source=.\sqlexpress;Initial Catalog=sircldb;Persist Security Info=True;User ID=sa;Password=rai11**";
+            Customer? customer = new Customer();
+            IDbConnection conn = new SqlConnection(constr);
+            customer = conn.Query<Customer>($"select * from customers where customerid={id}").SingleOrDefault();
+            if (customer != null)
+            {
+                return View(customer);
+            }
+            return View(null);
+        }
+        [HttpPost]
+        public IActionResult Edit(Customer customer)
+        {
+            string constr = @"Data Source=.\sqlexpress;Initial Catalog=sircldb;Persist Security Info=True;User ID=sa;Password=rai11**";
+
+            IDbConnection conn = new SqlConnection(constr);
+            string cmd = "update customers set firstname=@FirstName,lastname=@LastName,email=@Email where customerid=@CustomerId";
+            int affectedrows = conn.Execute(cmd, customer);
+            return RedirectToAction("Index");
+        }
+
+      
+        public IActionResult Delete(int id)
+        {
+            string constr = @"Data Source=.\sqlexpress;Initial Catalog=sircldb;Persist Security Info=True;User ID=sa;Password=rai11**";
+            Customer? customer = new Customer();
+            IDbConnection conn = new SqlConnection(constr);
+            int x= conn.Execute($"delete  from customers where customerid={id}");
+
+            return RedirectToAction("Index");
+        }
     }
 }
